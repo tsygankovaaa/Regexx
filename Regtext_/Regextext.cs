5d91774bcb557f0a64cda389f91bdf7e27d055e5
unknown <marichka_03@mail.ru>
2022-06-22 16:15:06 +0300

    rip

---
+++ Regtext_/Regextext.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Regtext_;
+
+namespace Regtext_
+{
+    public class Regextext
+    {
+        public bool regtext(string str)
+        {
+            Regex regex = new Regex("code");
+
+            return regex.IsMatch(str);
+        }
+        public string regreplace(string str)
+        {
+            string pattern = @"\W?(а)";
+            string replacement1 = "о";
+            return Regex.Replace(str, pattern, replacement1);
+        }
+        public int regciv(string str)
+        {
+            Regex regex = new Regex(@"\d");
+            return regex.Matches(str).Count;
+        }
+
+        public bool regmail(string email)// EMAIL
+        {
+            string pattern = @"^(?("")(""[^""]+?""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
+            @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9]{2,17}))$";
+
+            return Regex.IsMatch(email, pattern, RegexOptions.IgnoreCase);
+        }
+        public bool regsnils(string snils) //CНИЛС
+        {
+            
+            
+            string pattern = (@"\d{3}-\d{3}-\d{4}\s\d{2}$");
+            //Match match = regex.Match(snils);
+            return Regex.IsMatch(snils, pattern, RegexOptions.IgnoreCase);
+
+        }
+        public bool regnomer(string nomer)
+        {
+            string pattern = (@"(+7|8){1}9\d{9}$");
+            return Regex.IsMatch(nomer, pattern, RegexOptions.IgnoreCase);
+
+        }
+        //public class Class212
+        //{
+        //    //public double regperevod()
+        //    //{
+
+
+        //    //}
+
+            public double regperevod(double xy, double x)
+            {
+                //double xy = Convert.ToDouble(Console.ReadLine());
+                //double x = Convert.ToInt32(Console.ReadLine());
+                switch (xy)
+                {
+                    case 0:                                             //"Байты в килобайты" || "байты в килобайты":
+                        return x / 1024;
+                    case 1:                                              //"Байты в мегабайты || байты в мегабайты":
+                        return x / 1024 / 1024;
+                    case 2:                                              //"Байты в гигабайты || байты в гигабайты":
+                        return x / 1024 / 1024 / 1024;
+                    case 3:                                              //"Килобайты в мегабайты || килобайты в мегабайты":
+                        return x / 1024;
+                    case 4:                                               //"Килобайты в гигабайты || килобайты в гигабайты":
+                        return x / 1024 / 1024;
+                    case 5:                                               //"Мегабайты в гигабайты || мегабайты в гигабайты":
+                        return x * 1024;
+                }
+                return x;
+            }
+        //}
+
+        public static double itog;
+        public int regperevod2()
+        {
+            string xy = Convert.ToString(Console.ReadLine()).ToLower();
+            double x = Convert.ToDouble(Console.ReadLine());
+            //int itog;
+            if (xy == "байты в килобайты" || xy == "килобайты в байты")
+            {
+                return (int)(x / 1024);
+            }
+            else if (xy == "байты в мегабайты" || xy == "мегабайты в байты")
+            {
+               return (int)(x / 1024 / 1024);
+
+            }
+            else if (xy == "байты в гигабайты" || xy == "гигабайты в байты")
+            {
+                return (int)(x / 1024 / 1024 / 1024);
+            }
+            else if (xy == "килобайты в мегабайты" || xy == "мегабайты в килобайты")
+            {
+                return (int)(x / 1024);
+            }
+            else if (xy == "килобайты в гигабайты" || xy == "гигабайты в килобайты")
+            {
+                return (int)(x / 1024 / 1024);
+            }
+            else if (xy == "мегабайты в гигабайты" || xy == "гигабайты в мегабайты")
+            {
+                return (int)(x * 1024);
+            }
+            else
+            {
+                Console.WriteLine("Неверный ввод информации");
+            }
+            return (int)itog;
+            
+        }
+        public bool regpasport(string pasport) //ПАСПОРТ
+        {
+            string pattern = (@"\d{4}\s\d{6}$");
+            //Match match = regex.Match(snils);
+            return Regex.IsMatch(pasport, pattern, RegexOptions.IgnoreCase);
+        }
+        public int reguserid(string str)
+        {
+            Regex regex = new Regex(@"^user=(\d+)");
+            Match match = regex.Match(str);
+            int d = 0;
+            while (match.Success)
+            {
+                d = Convert.ToInt32(match.Groups[1].Value);
+                match = match.NextMatch();
+            }
+            return d;
+        }
+        public int regwcount(string str)
+        {
+            Regex regex = new Regex(@"(\w+\s*)");
+            Match match = regex.Match(str);
+            int d = 0;
+            if (match.Success)
+            {
+                d += Convert.ToInt32(regex.Matches(str).Count);
+                match = match.NextMatch();
+            }
+            return d;
+
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Regtext_;
+using static Regtext_.Regextext;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        Regextext regtext = new Regextext();
+        //Class212 cl212 = new Regextext.Class212();
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string str = "This is my code";
+            bool result = regtext.regtext(str);
+            Assert.AreEqual(true, result);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Regextext regtext = new Regextext();
+            string str = "class";
+            bool result = regtext.regtext(str);
+            Assert.AreEqual(false, result);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            string str = "мама";
+            string s = regtext.regreplace(str);
+            Assert.AreEqual("момо", s);
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            string str = "момо";
+            string s = regtext.regreplace(str);
+            Assert.AreEqual("момо", s);
+        }
+        [TestMethod]
+        public void TestMethod5()
+        {
+            string str = "пароль:77623";
+            int result = regtext.regciv(str);
+            Assert.AreEqual(5, result);
+        }
+        [TestMethod]
+        public void TestMethod6()
+        {
+            string str = "пароль:апельсин";
+            int result = regtext.regciv(str);
+            Assert.AreEqual(0, result);
+        }
+        [TestMethod]
+        public void TestMethod7()
+        {
+            string str = "tsygankova@mail.ru";
+            bool s = regtext.regmail(str);
+            Assert.AreEqual(true, s);
+        }
+        [TestMethod]
+        public void TestMethod8()//EMAIL
+        {
+            string str = "tsygankova";
+            bool s = regtext.regmail(str);
+            Assert.AreEqual(false, s);
+        }
+        [TestMethod]
+        public void TestMethod09()//СНИЛС
+        {
+            string str = "123-123-2343 23";
+            bool s = regtext.regsnils(str);
+            Assert.AreEqual(true, s);
+        }
+        [TestMethod]
+        public void TestMethod010()//НОМЕР
+        {
+            string str = "89088703057";
+            bool s = regtext.regnomer(str);
+            Assert.AreEqual(true, s);
+        }
+        [TestMethod]
+
+        public void TestMethod011()//НОМЕР
+        {
+            string str = "+79088703057";
+            bool s = regtext.regnomer(str);
+            Assert.AreEqual(true, s);
+        }
+        [TestMethod]
+        public void TestMethod012()//ПАСПОРТ
+        {
+            string str = "7116 567857";
+            bool s = regtext.regpasport(str);
+            Assert.AreEqual(true, s);
+        }
+        [TestMethod]
+        public void TestMethod013()
+        {
+            double str = Convert.ToDouble(regtext.regperevod(0, 1024));
+            Assert.AreEqual(1, str);
+        }
+
+        [TestMethod]
+        public void TestMethod9()
+        {
+            string str = "user=5";
+            var result = regtext.reguserid(str);
+            Assert.AreEqual(5, result);
+        }
+        [TestMethod]
+        public void TestMethod10()
+        {
+            string res = Convert.ToString(regtext.reguserid("user=12"));
+            Assert.AreEqual("12", res);
+        }
+
+        [TestMethod]
+        public void TestMethod11()
+        {
+
+        }
+    }
+
+}
